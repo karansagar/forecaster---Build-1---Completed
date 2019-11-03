c56c847e85a42e273752e5235e08f6fd47494cc2
karan <karan.sagar1028@gmail.com>
2019-11-03 22:56:02 +0530

    Added Initial Cocoapods, Created MVC Structure (Incomplete Logics. )

---
+++ forecaster/Controller/ChangeCityViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ChangeCityViewController.swift
+//  forecaster
+//
+//  Created by Karan Sagar on 03/11/19.
+//  Copyright © 2019 Karan. All rights reserved.
+//
+
+import UIKit
+
+class ChangeCityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
