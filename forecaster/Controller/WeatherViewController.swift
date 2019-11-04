85c0a08f7f2e0d44e8a5cf90f83ddecbe80224c0
karan <karan.sagar1028@gmail.com>
2019-11-04 16:35:11 +0530

    Connected Buttons. Labels, TextField.

--- forecaster/Controller/WeatherViewController.swift
+++ forecaster/Controller/WeatherViewController.swift
@@ -7,8 +7,25 @@
 //
 
 import UIKit
+import CoreLocation
+import SwiftyJSON
+import Alamofire
 
 class WeatherViewController: UIViewController {
+    
+    //Constants
+    let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
+    let APP_ID = "1cd200cd6e6f7012adbbcba1768b63a7"
+    
+    //TODO: Declare instance variables here
+
+    
+    // linked IBOutlets
+    
+    @IBOutlet weak var weatherIcon: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
